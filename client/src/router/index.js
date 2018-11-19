3c906b96f55ea192b0a4af2187638a1ce5ec66b6
JS <jrseasly@fbi.gov>
2018-11-19 16:46:22 +0700

    server and client setup

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/registered',
+      name: 'register',
+      component: Register
     }
   ]
 })
--- server/src/app.js
+++ server/src/app.js
@@ -15,4 +15,10 @@ app.get('/status', (req, res) => {
   })
 })
 
+app.post('/register', (req, res) => {
+  res.send({
+    message: 'user registered'
+  })
+})
+
 app.listen(process.env.PORT || 8081)
